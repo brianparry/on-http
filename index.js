c3ada96b88d759d6d559baa9f1135dd50d82e014
Brian Parry <brian.parry@emc.com>
2016-01-20 20:21:29 -0500

    Add Ted's auth service

--- index.js
+++ index.js
@@ -44,6 +44,7 @@ function onHttpContextFactory(di, directory) {
 
         injectables: _.flattenDeep([
             helper.requireGlob(__dirname + '/lib/api/1.1/*.js'),
+            helper.requireGlob(__dirname + '/lib/api/auth/*.js'),
             helper.requireGlob(__dirname + '/lib/services/**/*.js'),
             helper.requireGlob(__dirname + '/lib/serializables/**/*.js'),
             require('./app'),
